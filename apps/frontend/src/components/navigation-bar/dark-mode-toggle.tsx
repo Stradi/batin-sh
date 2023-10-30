c0b569f0e8887fbc140552d7394ea713e1f74c7d
Batin Evirgen <batinevirgen@gmail.com>
2023-10-30 16:22:21 +0300

    fix: add animations to DarkModeToggle

--- apps/frontend/src/components/navigation-bar/dark-mode-toggle.tsx
+++ apps/frontend/src/components/navigation-bar/dark-mode-toggle.tsx
@@ -1,13 +1,42 @@
 'use client';
 
+/* eslint-disable react/jsx-sort-props -- In `motion` components, sorting alphabetically looks bad. */
+/* TODO: This component throws an error to Next.js console during SSR. I don't know why, fix that. */
+
+import { MoonIcon, SunIcon } from 'lucide-react';
+import { AnimatePresence, motion } from 'framer-motion';
 import useTheme from '@/hooks/use-theme';
+
+const MotionSunIcon = motion(SunIcon);
+const MotionMoonIcon = motion(MoonIcon);
+
+const RotateAmount = 30;
+const TranslationAmount = 3;
 
 export default function DarkModeToggle() {
   const [theme, _, toggleTheme] = useTheme();
+  const Component = theme === 'dark' ? MotionSunIcon : MotionMoonIcon;
 
   return (
     <button onClick={toggleTheme} type="button">
-      {theme === 'dark' ? '🌚' : '🌞'}
+      <AnimatePresence>
+        <Component
+          className="w-5 h-5"
+          strokeWidth={1.75}
+          initial={{
+            translateX: -TranslationAmount,
+            rotateZ: -RotateAmount,
+          }}
+          animate={{
+            translateX: 0,
+            rotateZ: 0,
+          }}
+          exit={{
+            translateX: TranslationAmount,
+            rotateZ: RotateAmount,
+          }}
+        />
+      </AnimatePresence>
     </button>
   );
 }
