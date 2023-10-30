bf309bed0d4d693f9d42eb3d8283534c1fe001a3
Batin Evirgen <batinevirgen@gmail.com>
2023-10-30 13:21:25 +0300

    chore: remove stupid eslint rules

--- apps/frontend/src/components/container.tsx
+++ apps/frontend/src/components/container.tsx
@@ -5,7 +5,6 @@ type Props = ComponentPropsWithoutRef<'div'>;
 
 export default function Container({ children, className, ...props }: Props) {
   return (
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call -- Weird errors
     <div className={cn('container mx-auto max-w-3xl px-4', className)} {...props}>
       {children}
     </div>
