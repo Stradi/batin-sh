bd8101ef427ba4f0a7a1a92093c4457f45e0a474
Batin Evirgen <batinevirgen@gmail.com>
2023-11-01 22:15:40 +0300

    feat: add `hideIcon` prop to `BadgeLink` component

--- apps/frontend/src/components/badge-link.tsx
+++ apps/frontend/src/components/badge-link.tsx
@@ -3,15 +3,17 @@ import { type ComponentPropsWithoutRef } from 'react';
 import { ExternalLinkIcon } from 'lucide-react';
 import Badge from './badge';
 
-type Props = ComponentPropsWithoutRef<typeof Link>;
+type Props = ComponentPropsWithoutRef<typeof Link> & {
+  hideIcon?: boolean;
+};
 
-export default function BadgeLink({ children, ...props }: Props) {
+export default function BadgeLink({ hideIcon, children, ...props }: Props) {
   const isExternal = (props.href as string).startsWith('http');
 
   return (
     <Link {...props}>
       <Badge className="hover:bg-neutral-200 dark:hover:border-neutral-600 dark:hover:bg-neutral-700">
-        {children} {isExternal ? <ExternalLinkIcon className="h-3 w-3" strokeWidth={2.25} /> : null}
+        {children} {!hideIcon && isExternal ? <ExternalLinkIcon className="h-3 w-3" strokeWidth={2.25} /> : null}
       </Badge>
     </Link>
   );
