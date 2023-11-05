f42ca581377bc7c44e34ac8fe44d8d134e0db79e
Batin Evirgen <batinevirgen@gmail.com>
2023-11-05 17:26:29 +0300

    feat: update blog layout

--- apps/frontend/src/app/blog/[slug]/page.tsx
+++ apps/frontend/src/app/blog/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation';
 import { allPosts } from 'contentlayer/generated';
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import { toRelativeDate } from '@/utils/date';
 
 export function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -21,16 +22,30 @@ type Props = {
   params: { slug: string };
 };
 
+// TODO: Stats
 export default function Page({ params }: Props) {
   const post = getPost(params.slug);
   const MDXContent = useMDXComponent(post.body.code);
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <main>
+    <article>
+      <header className="mb-8 space-y-1">
+        <h1 className="text-4xl text-black dark:text-white">{post.title}</h1>
+        <div className="flex items-center gap-2 text-neutral-500">
+          <p>
+            <time dateTime={post.publishedAt}>{toRelativeDate(post.publishedAt)}</time>
+          </p>
+          &middot;
+          <p>16,352 views</p>
+          &middot;
+          <p>6,243 likes</p>
+          &middot;
+          <p>12 comments</p>
+        </div>
+      </header>
+      <main className="prose prose-neutral dark:prose-invert prose-pre:my-[unset] max-w-none">
         <MDXContent />
       </main>
-    </div>
+    </article>
   );
 }
---
+++ apps/frontend/src/utils/date.ts
@@ -0,0 +1,24 @@
+export function toRelativeDate(date: string | Date) {
+  const relativeFormatter = new Intl.RelativeTimeFormat('en-us');
+
+  const DIVISIONS = [
+    { amount: 60, name: 'seconds' },
+    { amount: 60, name: 'minutes' },
+    { amount: 24, name: 'hours' },
+    { amount: 7, name: 'days' },
+    { amount: 4.34524, name: 'weeks' },
+    { amount: 12, name: 'months' },
+    { amount: Number.POSITIVE_INFINITY, name: 'years' },
+  ];
+
+  let duration = (new Date(date).getTime() - Date.now()) / 1000;
+
+  for (const division of DIVISIONS) {
+    if (Math.abs(duration) < division.amount) {
+      return relativeFormatter.format(Math.round(duration), division.name as Intl.RelativeTimeFormatUnit);
+    }
+    duration /= division.amount;
+  }
+
+  return relativeFormatter.format(Math.round(duration), 'years');
+}
