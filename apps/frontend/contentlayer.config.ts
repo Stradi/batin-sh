620f4e34324cb4070663c71ab3f8192015fe20e5
Batin Evirgen <batinevirgen@gmail.com>
2023-11-05 16:49:38 +0300

    feat: implement syntax highlighting for code blocks

--- apps/frontend/contentlayer.config.ts
+++ apps/frontend/contentlayer.config.ts
@@ -1,7 +1,34 @@
 import { makeSource } from 'contentlayer/source-files';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import type { Options } from 'rehype-pretty-code';
+import rehypePrettyCode from 'rehype-pretty-code';
 import { Post } from './_content/defs/post';
 
+const rehypePrettyCodeOptions: Partial<Options> = {
+  theme: {
+    dark: 'one-dark-pro',
+    light: 'min-light',
+  },
+  keepBackground: false,
+};
+
 export default makeSource({
+  disableImportAliasWarning: true,
   contentDirPath: './_content',
   documentTypes: [Post],
+  mdx: {
+    // TODO: add Remark GFM plugin
+    // https://github.com/contentlayerdev/contentlayer/issues/558
+    rehypePlugins: [
+      [rehypePrettyCode, rehypePrettyCodeOptions],
+      [rehypeSlug],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behaviour: 'wrap',
+        },
+      ],
+    ],
+  },
 });
