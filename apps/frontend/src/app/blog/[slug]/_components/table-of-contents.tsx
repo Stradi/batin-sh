db7b45f7d3c40698631cbb679f6c01d5c34b932c
Batin Evirgen <batinevirgen@gmail.com>
2023-11-06 22:09:12 +0300

    fix: adjust colors of table of contents section

--- apps/frontend/src/app/blog/[slug]/_components/table-of-contents.tsx
+++ apps/frontend/src/app/blog/[slug]/_components/table-of-contents.tsx
@@ -12,13 +12,13 @@ type Props = ComponentPropsWithoutRef<'aside'> & {
 export default function TableOfContents({ items, ...props }: Props) {
   return (
     <aside {...props}>
-      <p className="mb-2 text-sm uppercase text-neutral-400 dark:text-neutral-500 md:dark:text-neutral-700">
+      <p className="mb-2 text-sm uppercase text-neutral-500 dark:text-neutral-500 md:dark:text-neutral-600">
         On this page
       </p>
-      <ul className="space-y-1 text-neutral-500 [&>*]:block dark:[&>*]:text-neutral-400 md:dark:[&>*]:text-neutral-600">
+      <ul className="space-y-1 text-neutral-600 [&>*]:block dark:[&>*]:text-neutral-400 md:dark:[&>*]:text-neutral-500">
         {items.map((item) => (
           <Link
-            className="text-sm transition duration-100 hover:text-neutral-800 hover:underline md:dark:hover:text-neutral-400"
+            className="text-sm transition duration-100 hover:text-neutral-900 hover:underline md:dark:hover:text-neutral-300"
             href={`#${item.slug}`}
             key={item.slug}
             style={{
