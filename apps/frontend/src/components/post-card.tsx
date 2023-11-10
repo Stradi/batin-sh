0e54e921506d7d85bb521f69c034cb8fef85796b
Batin Evirgen <batinevirgen@gmail.com>
2023-11-10 22:51:10 +0300

    fix: add a horizontal line in post card

--- apps/frontend/src/components/post-card.tsx
+++ apps/frontend/src/components/post-card.tsx
@@ -9,10 +9,15 @@ type Props = {
 
 export default function PostCard({ post, excludeYearInDate }: Props) {
   return (
-    <Link className="group flex items-center justify-between gap-4" href={`/blog/${post.slug}`} title={post.title}>
+    <Link
+      className="group relative flex items-center justify-between gap-4"
+      href={`/blog/${post.slug}`}
+      title={post.title}
+    >
       <p className="truncate text-neutral-600 transition duration-100 group-hover:text-neutral-800 group-hover:underline dark:text-neutral-400 dark:group-hover:text-neutral-200">
         {post.title}
       </p>
+      <div className="h-1 grow rounded-sm bg-black/[0.025] opacity-0 transition duration-100 group-hover:opacity-100 dark:bg-white/[0.025]" />
       <time className="shrink-0 font-mono text-sm text-amber-600 dark:text-amber-500">
         {format(post.publishedAt, excludeYearInDate)}
       </time>
