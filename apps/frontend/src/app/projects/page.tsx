5c1aa5a9e77ca00d6f3c27082b97d08b5b8a9208
Batin Evirgen <batinevirgen@gmail.com>
2024-01-28 23:51:55 +0300

    feat: sort projects from newest to oldest

--- apps/frontend/src/app/projects/page.tsx
+++ apps/frontend/src/app/projects/page.tsx
@@ -1,21 +1,27 @@
-import { allProjects } from 'contentlayer/generated';
-import type { Metadata } from 'next';
 import ProjectCard from '@/components/project-card';
 import { normalizeProject } from '@/utils/contentlayer';
+import { allProjects } from 'contentlayer/generated';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Projects",
+  title: 'Projects',
   description: "Things I've built.",
-}
+};
 
 export default function Page() {
   return (
     <div className="space-y-12">
       <h1 className="text-2xl md:text-4xl">Projects</h1>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        {allProjects.map((project) => (
-          <ProjectCard key={project.slug} project={normalizeProject(project)} />
-        ))}
+        {allProjects
+          .sort((a, b) => a.publishedAt.localeCompare(b.publishedAt))
+          .toReversed()
+          .map((project) => (
+            <ProjectCard
+              key={project.slug}
+              project={normalizeProject(project)}
+            />
+          ))}
       </div>
     </div>
   );
